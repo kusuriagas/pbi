daaedcb7ec11298aee191ab16c6c901bc165054e
EDISON <91626034+kusuriagas@users.noreply.github.com>
2022-02-24 21:10:28 -0500

    Create script.m

---
+++ script.m
@@ -0,0 +1,12 @@
+Calendario
+
+let
+    Origen = List.Dates(
+                        #date(2017,01,01), //Fecha de Inicio
+                        365*3,             //Fecha para cada dia por los proximo 3 años
+                        #duration(1,0,0,0) // Especificar la duracion del periodo 1 = dia, 0 =hora, 0= minutos, 0=segundos
+    )
+in
+    Origen
+    
+    
